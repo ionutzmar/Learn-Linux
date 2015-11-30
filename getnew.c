12de66b6594c6327380acda4bf751830713cc619
root <ionutzmar@gmail.com>
2015-11-30 20:29:55 +0200

    Read event

---
+++ getnew.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/input.h>
+
+#define MOUSEFILE "/dev/input/event4"
+
+int main()
+{
+	int fd;
+	struct input_event ie;
+
+	if((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
+		perror("opening device");
+		exit(EXIT_FAILURE);
+	}
+
+	while(read(fd, &ie, sizeof(struct input_event))) {
+		printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
+		       ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
+	}
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -4,3 +4,5 @@ run:
 	sudo ./moveMouse
 getCoordinates:
 	sudo gcc mouseCoordinates.c -o "getCoordinates" -Wall
+getnew:
+	sudo gcc getnew.c -o "getnew" -Wall
